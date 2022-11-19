4827be2d9066fd656483c8b535750bf2481ce612
Sera Susan Koshy <108021864+serasusan@users.noreply.github.com>
2022-11-19 22:33:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,10 +23,21 @@ renderer.render(scene, camera);
 // Torus
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0x32174d });
+const material = new THREE.MeshStandardMaterial({ color: 0x00B1B0 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+// const donutTexture = new THREE.MeshStandardMaterial({color:0x39174d});
+//const wrapTexture = new THREE.TextureLoader().load('donut.jpg');
+const donut = new THREE.Mesh(
+      new THREE.TorusGeometry(10,3,16,100),
+      new THREE.MeshStandardMaterial({
+          color:0xFEC84D,
+          //  normalMap : wrapTexture
+      })
+);
+scene.add(donut);
 
 // Lights
 
@@ -59,7 +70,7 @@ function addStar() {
 
 function addCube1(){
   
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const geometry = new THREE.BoxGeometry(3, 2, 1);
   const cubtex1 = new THREE.TextureLoader().load('cube.jpeg');
 
   const material = new THREE.MeshBasicMaterial({ map:cubtex1 });
@@ -87,16 +98,22 @@ function addCube2(){
 
 
 
-Array(50).fill().forEach(addStar);
-Array(50).fill().forEach(addCube1);
-Array(50).fill().forEach(addCube2);
+Array(100).fill().forEach(addStar);
+Array(10).fill().forEach(addCube1);
+Array(10).fill().forEach(addCube2);
 
 
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('bg.jpg');
+const spaceTexture = new THREE.TextureLoader().load('bg.jpeg');
 scene.background = spaceTexture;
+
+// function changeBackground(color) {
+//   document.body.style.background = color;
+// }
+
+// window.addEventListener("load",function() { changeBackground('red') });
 // document.body.style.backgroundColor = "#AA0000";
 
 // Avatar
@@ -161,6 +178,11 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+ 
+  
+  donut.rotation.x += 0.021;
+  donut.rotation.y += 0.0005;
+  donut.rotation.z += 0.01;
 
   
 
